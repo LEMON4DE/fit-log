ac5a994eda5f86a95300b48866eb87d82fb353a9
LEMON4DE <111743489+LEMON4DE@users.noreply.github.com>
2023-04-26 18:03:46 +0900

    Resolved #1

--- model/db.php
+++ model/db.php
@@ -1,6 +1,9 @@
 <?php
 class DB {
     protected static function connectToDB() {
-        
+        $db_username = isset($_SERVER['FIT_LOG_DB_USERNAME']) ? $_SERVER['FIT_LOG_DB_USERNAME'] : 'root';
+        $db_password = $_SERVER['FIT_LOG_DB_PASSWORD'];
+
+        return new PDO('mysql:host=localhost;dbname=fit_log;charset=utf8', $db_username, $db_password);
     }
 }
