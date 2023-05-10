d0c6c66e5f2bff9d25f0f22360bfcefa12adc554
LEMON4DE <111743489+LEMON4DE@users.noreply.github.com>
2023-05-10 21:04:14 +0900

    Centered the logo in the header using grid. Resolves #18

--- view/header.php
+++ view/header.php
@@ -6,19 +6,19 @@ $_GET["action"] = $_GET["action"] ?? "";
 
 <header>
     <div id="header-main">
-        <span>FIT LOG</span>
-        <?php if(isset($_SESSION['user']) && $_SESSION['user'] !== false) { ?>
-            <div>
-                <span>Hi, <?= $_SESSION['user'][2] ?></span>
-            </div>
+        <span id="header-logo">FIT LOG</span>
+    <?php if(isset($_SESSION['user']) && $_SESSION['user'] !== false) { ?>
+        <div id="profile-container">
+            <span>Hi, <?= $_SESSION['user'][2] ?></span>
             <form method="POST" action="">
                 <button type="submit" name="sign-out">Sign out</button>
             </form>
-        <?php if(isset($_POST['sign-out'])){
-            session_unset();
-            session_destroy();
-            header('location: ' . HOME);
-        }} ?>
+        </div>
+    <?php if(isset($_POST['sign-out'])){
+        session_unset();
+        session_destroy();
+        header('location: ' . LOG_IN);
+    }} ?>
     </div>
     <nav>
         <ul>
